8c740c7b27f6fd3bc45a1f1181010f4ef2e58c7c
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-11-05 20:19:02 -0700

    feat: added openzeppelin contracts and minor fixes.

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,27 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import '@nomiclabs/hardhat-etherscan';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const config: HardhatUserConfig = {
+const config = {
   solidity: "0.8.17",
+  networks: {
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/1dc931d842b449c0a674b754ee15aafd",
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY]
+    },
+    matic: {
+      url: "https://polygon-rpc.com/",
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY]
+    },
+    matic_mumbai: {
+      url: "https://matic-mumbai.chainstacklabs.com",
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
 };
 
 export default config;
