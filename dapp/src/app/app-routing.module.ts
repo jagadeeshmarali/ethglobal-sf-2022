1f14e2881a6763939f48ed27f33881bba59122e8
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-11-05 21:49:49 -0700

    feat: modified the app component elements by moduling.

--- dapp/src/app/app-routing.module.ts
+++ dapp/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
+import { UploadPage } from './pages/uploads/upload.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomePage } from './pages/home/home.component';
+import { NFTPage } from './pages/nfts/nft.component';
 
 
 const routes: Routes = [
-  { path: "", component: HomePage, pathMatch: 'full' }
+  // { path: "", component: HomePage, pathMatch: 'full' },
+  { path: "", component: NFTPage, pathMatch: 'full' },
+  { path: "uploads", component: UploadPage, pathMatch: 'full' }
 
 
 
--- dapp/src/app/app.component.ts
+++ dapp/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SessionService } from './session.service';
+declare const window: any;
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kripton';
+  sessionFetched = false;
+  title = 'web3-angular-starter-kit';
+  constructor(public sessionService: SessionService,) {
+    this.init();
+  }
+  init() {
+    if (typeof window.ethereum == 'undefined') {
+      console.log('Please use a dapp browser like mist or MetaMask plugin for chrome');
+    }
+    else {
+      console.log('Ethereum browser detected');
+      this.fetchSession();
+    }
+  }
+  fetchSession() {
+    this.sessionFetched = false;
+    this.sessionService.getAccounts().then(() => {
+      console.log("fetching accounts ...");
+      this.sessionService.fetchCurrentUser(this.sessionService.accounts[0]).then(() => {
+        this.sessionFetched = true;
+      });
+
+    });
+  }
+  async connectWallet() {
+    try {
+      await window.ethereum.enable();
+      this.fetchSession();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async disconnect() {
+    console.log("=============")
+    // await window.ethereum.;
+  }
 }
