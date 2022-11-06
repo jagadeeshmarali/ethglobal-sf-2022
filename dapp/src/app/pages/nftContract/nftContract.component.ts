04b1de5d2b424bd234ea2dd1c903f356bce8b67d
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-11-06 02:37:08 -0800

    nft contract added.

---
+++ dapp/src/app/pages/nftContract/nftContract.component.ts
@@ -0,0 +1,76 @@
+import { UploadService } from './../../services/upload.service';
+import { RentableNftService } from './../../services/rentableNft.service';
+import { Component } from "@angular/core";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
+import { SessionService } from '../../session.service';
+
+@Component({
+  selector: "nftcontract-page",
+  templateUrl: "./nftContract.component.html"
+})
+export class NftContractPage {
+  index = 0
+  toAddress = "";
+  uri = ""
+  myNftCollectables = []
+  public fileBuffer: any;
+  public fileName;
+  address = "";
+  mintbutton = false;
+
+  constructor(
+    private modalService: NgbModal,
+    private rentableNftService: RentableNftService,
+    private route: ActivatedRoute,
+    public uploadService: UploadService,
+    public sanitizer: DomSanitizer,
+    public sessionService: SessionService
+  ) {
+    this.uploadService.getMyUploads(this.sessionService.accounts[0]).then(x => {
+      console.log(x)
+      console.log(this.sessionService.accounts[0])
+      this.myNftCollectables = x;
+      // for (let i = 0; i < this.myNftCollectables.length; i++) {
+      //   this.uploadService.downloadFileToVariable(this.myNftCollectables[i].storagePath).then(data => {
+      //     this.myNftCollectables[i]["image"] = 'data:image/png;base64,' + data.toString('base64');
+      //   })
+      // }
+      // console.log(this.myNftCollectables)
+    })
+
+  }
+  openLg(content: any) {
+    this.modalService.open(content, { size: 'lg' });
+  }
+  deployContract() {
+    this.rentableNftService.mintNFT(this.index, this.toAddress, this.uri).then(x => {
+      console.log(x);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+  ngOnInit() {
+    this.index = this.route.snapshot.params["id"];
+    this.address = this.route.snapshot.params["contract"];
+  }
+  async handleFileInput(event) {
+    const file: File = (event.target.files as FileList)[0];
+    console.log(file.name);
+    this.fileName = file.name
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onload = (e) => {
+      this.fileBuffer = Buffer.from(reader.result);
+    }
+  }
+  upload() {
+    this.uploadService.upload('', this.fileName, this.fileBuffer).then(x => {
+      console.log(x);
+      this.uri = x;
+      this.mintbutton = true;
+    })
+  }
+}
