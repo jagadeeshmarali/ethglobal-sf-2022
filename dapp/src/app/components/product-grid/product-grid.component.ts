6c4c88a03db7296ed90371ec6f9df609243f2ab6
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-11-05 21:50:37 -0700

    feat: modified the product grid json.

--- dapp/src/app/components/product-grid/product-grid.component.ts
+++ dapp/src/app/components/product-grid/product-grid.component.ts
@@ -11,197 +11,197 @@ export class ProductGridComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  
+
   products = [
+    {
+      image: "https://solanart.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdemo%2Fimage%2Ffetch%2Fh_500%2Fhttps%3A%2F%2Farweave.net%2FcKhNqEPVk-Ll51TvBCx5RPc5NvJUo45ZbIHfjDv83Cc&w=1920&q=75",
+      title: "Bonorum et Malorum",
+      url: "admin/ecom-product-detail",
+      price: "$761.00",
+      ratings_class: [
         {
-          image: "assets/images/product/1.jpg",
-          title: "Bonorum et Malorum",
-          url: "admin/ecom-product-detail",
-          price: "$761.00",
-		  ratings_class: [
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            },
-          ]
+          icon_class: "fa fa-star",
         },
         {
-          image: "assets/images/product/2.jpg",
-          title: "Striped Dress",
-          url: "admin/ecom-product-detail",
-          price: "$159.00",
-		  ratings_class: [
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            }
-		  ]
-			
+          icon_class: "fa fa-star",
         },
         {
-          image: "assets/images/product/3.jpg",
-          title: "BBow polka-dot",
-          url: "admin/ecom-product-detail",
-          price: "$357.00",
-		  ratings_class: [
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            },
-		  ]
+          icon_class: "fa fa-star",
         },
         {
-          image: "assets/images/product/4.jpg",
-          title: "Z Product 360",
-          url: "admin/ecom-product-detail",
-          price: "$654.00",
-		  ratings_class: [
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            },
-		  ]
+          icon_class: "fa fa-star-half-empty",
         },
         {
-          image: "assets/images/product/5.jpg",
-          title: "Chair Grey",
-          url: "admin/ecom-product-detail",
-          price: "$369.00",
-		  ratings_class: [
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            },
-		  ]
+          icon_class: "fa fa-star-half-empty",
+        },
+      ]
+    },
+    {
+      image: "https://solanart.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdemo%2Fimage%2Ffetch%2Fh_500%2Fhttps%3A%2F%2Farweave.net%2Fjm6wD4dL6h-ngCdATOmufSbaYLXsAzCseyOveXnkYk8&w=1920&q=75",
+      title: "Striped Dress",
+      url: "admin/ecom-product-detail",
+      price: "$159.00",
+      ratings_class: [
+        {
+          icon_class: "fa fa-star",
         },
         {
-          image: "assets/images/product/6.jpg",
-          title: "fox sake withe",
-          url: "admin/ecom-product-detail",
-          price: "$245.00",
-		  ratings_class: [
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            },
-		  ]
+          icon_class: "fa fa-star",
         },
         {
-          image: "assets/images/product/7.jpg",
-          title: "Back Bag",
-          url: "admin/ecom-product-detail",
-          price: "$364.00",
-		  ratings_class: [
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            },
-		  ]
+          icon_class: "fa fa-star",
         },
         {
-          image: "assets/images/product/1.jpg",
-          title: "FLARE DRESS",
-          url: "admin/ecom-product-detail",
-          price: "$548.00",
-		  ratings_class: [
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            },
-            {
-              icon_class:"fa fa-star-half-empty",
-            },
-		  ]
+          icon_class: "fa fa-star-half-empty",
         },
-      
-      
-  
-    
-	];
+        {
+          icon_class: "fa fa-star-half-empty",
+        }
+      ]
+
+    },
+    {
+      image: "https://solanart.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdemo%2Fimage%2Ffetch%2Fh_500%2Fhttps%3A%2F%2Farweave.net%2FcKhNqEPVk-Ll51TvBCx5RPc5NvJUo45ZbIHfjDv83Cc&w=1920&q=75",
+      title: "BBow polka-dot",
+      url: "admin/ecom-product-detail",
+      price: "$357.00",
+      ratings_class: [
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star-half-empty",
+        },
+        {
+          icon_class: "fa fa-star-half-empty",
+        },
+      ]
+    },
+    {
+      image: "https://solanart.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdemo%2Fimage%2Ffetch%2Fh_500%2Fhttps%3A%2F%2Farweave.net%2FcKhNqEPVk-Ll51TvBCx5RPc5NvJUo45ZbIHfjDv83Cc&w=1920&q=75",
+      title: "Z Product 360",
+      url: "admin/ecom-product-detail",
+      price: "$654.00",
+      ratings_class: [
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star-half-empty",
+        },
+        {
+          icon_class: "fa fa-star-half-empty",
+        },
+      ]
+    },
+    {
+      image: "https://solanart.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdemo%2Fimage%2Ffetch%2Fh_500%2Fhttps%3A%2F%2Farweave.net%2Fg38wnbhXA2z-60HNH6qnaUm_CyRAntMTB1RFwkUO8zI&w=1920&q=75",
+      title: "Chair Grey",
+      url: "admin/ecom-product-detail",
+      price: "$369.00",
+      ratings_class: [
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star-half-empty",
+        },
+        {
+          icon_class: "fa fa-star-half-empty",
+        },
+      ]
+    },
+    {
+      image: "https://solanart.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdemo%2Fimage%2Ffetch%2Fh_500%2Fhttps%3A%2F%2Fbafybeicopqcbvgbwoooetedajjpvqzv7v7q6b4225rgr4sqehlfxgxwa2e.ipfs.nftstorage.link%2F7460.png%3Fext%3Dpng&w=1920&q=75",
+      title: "fox sake withe",
+      url: "admin/ecom-product-detail",
+      price: "$245.00",
+      ratings_class: [
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star-half-empty",
+        },
+        {
+          icon_class: "fa fa-star-half-empty",
+        },
+      ]
+    },
+    {
+      image: "https://solanart.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdemo%2Fimage%2Ffetch%2Fh_500%2Fhttps%3A%2F%2Fbafybeifge5aw4dynsfxl5n4rgde66djy7so4bjccxyvidjptqeq7ymoo4u.ipfs.nftstorage.link%2F9483.png%3Fext%3Dpng&w=1920&q=75",
+      title: "Back Bag",
+      url: "admin/ecom-product-detail",
+      price: "$364.00",
+      ratings_class: [
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star-half-empty",
+        },
+        {
+          icon_class: "fa fa-star-half-empty",
+        },
+      ]
+    },
+    {
+      image: "https://solanart.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdemo%2Fimage%2Ffetch%2Fh_500%2Fhttps%3A%2F%2Fbafybeihds3yzrk4vt6wyhhpxpyisyq2h6nygep6njwesdaeyt3hy5cf4wm.ipfs.nftstorage.link%2F9566.png%3Fext%3Dpng&w=1920&q=75",
+      title: "FLARE DRESS",
+      url: "admin/ecom-product-detail",
+      price: "$548.00",
+      ratings_class: [
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star",
+        },
+        {
+          icon_class: "fa fa-star-half-empty",
+        },
+        {
+          icon_class: "fa fa-star-half-empty",
+        },
+      ]
+    },
+
+
+
+
+  ];
 
 }
