2998fb6f5e12457116a02f189315235e36a3bae6
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-11-05 21:52:46 -0700

    modified the header footer and nav bar.

--- dapp/src/app/shared/header/header.component.ts
+++ dapp/src/app/shared/header/header.component.ts
@@ -1,27 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import {NgbDropdownConfig} from '@ng-bootstrap/ng-bootstrap';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
+import { SessionService } from '../../session.service';
+import { trim } from '../../utils';
+
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-	
+
 	toggleChat: boolean = true;
 	toggleSingle: boolean = true;
-	
-	constructor() { }
-	
+
+
+
 	ngOnInit(): void {
 	}
-	
-	
+
+
 	togglechatbar() {
 		this.toggleChat = !this.toggleChat;
 	}
 	singleChatWindow() {
 		this.toggleSingle = !this.toggleSingle;
+	}
+	public trim = trim;
+	@Output() connectEvent = new EventEmitter<any>();
+
+	constructor(public sessionService: SessionService) { }
+
+	emitconnectEvent() {
+		this.connectEvent.emit();
 	}
 
 }
